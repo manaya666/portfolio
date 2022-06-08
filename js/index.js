069ae4d05f73f884551212c43e489032b6fdacff
manaya666 <92067002+manaya666@users.noreply.github.com>
2022-06-08 10:59:09 +0900

    a

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 function load_effect() {
-    let element = document.getElementsByClassName('')
+    let element = document.getElementsByClassName('top__title');
+    if (!element) return;
+    for (let i = 0; i < element.length; i++) {
+        element[i].classList.add('is-show');
+    }
 }
+setTimeout(load_effect, 600);
